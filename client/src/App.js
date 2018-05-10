46866b30d2b77b67861eb2b4e7268a200ecfbc13
Nino <nino.zumstein@gmail.com>
2018-05-10 16:22:16 +0200

    added client skeleton

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import classes from './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={classes.App}>
+        <header className={classes.App_header}>
+          <img src={logo} className={classes.App_logo} alt="logo" />
+          <h1 className={classes.App_title}>Welcome to React (with css Modules)</h1>
+        </header>
+        <p className={classes.App_intro}>
+          To get started, edit <code>src/App.js</code> and save to reload. 
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
